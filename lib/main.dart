b21a57613a0178f1684c3d823eb2ec32657dd5c8
Abdelghfarkhirallah <140082083+Abdelghfarsalah@users.noreply.github.com>
2024-02-29 17:06:18 +0200

    start build project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const travelapp());
+}
+
+class travelapp extends StatelessWidget {
+  const travelapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
